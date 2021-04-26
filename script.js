dcf5dd32c325a615d81cb346b8fe8fde37ed2620
colmo0912 <colmo0912@gmail.com>
2021-04-26 18:09:23 +0200

    fine esercizio 2

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 //esercizio uno
 //creo un array con 5 oggetti bici
 
-var gruppoBici = [
+const gruppoBici = [
 
     
     {nome: "bici1" , peso: 30},
@@ -31,7 +31,7 @@ pesiBici.sort();
 console.log(pesiBici)
 
 //recupero il più piccolo usando la posizione 0
-var pesoPiuBasso = pesiBici[0]
+const pesoPiuBasso = pesiBici[0]
 console.log(`Il peso più basso tra tutte le bici è ${pesoPiuBasso} kg`)
 
 
@@ -48,5 +48,37 @@ var gruppoSquadre = [
     {nome: "squadra5" , punti: 0, falliSubiti: 0},
 
 ]
+
+//ciclo per prendere gli indici di punti al quale assegno un valore random
+
+for (let i = 0; i<gruppoSquadre.length; i++){
+     var puntiSingolaSquadra = Math.floor(Math.random() * 40) + 20
+        gruppoSquadre[i].punti = puntiSingolaSquadra 
+    
+}
+
+//ciclo per prendere gli indici di fallisubiti al quale assegno un valore random
+
+for (let i = 0; i<gruppoSquadre.length; i++){
+       var falliSingolaSquadra = Math.floor(Math.random() * 20) + 5
+        gruppoSquadre[i].falliSubiti = falliSingolaSquadra 
+    
+}
+
+var nomi_Squadre = [];
+for (let i =0; i<gruppoSquadre.length; i++) {
+    var nome = gruppoSquadre[i].nome;
+    var falliSubiti = gruppoSquadre[i].falliSubiti;
+    console.log(nome);
+    console.log(falliSubiti);
+
+    nomi_Squadre.push({nome: nome , falliSubiti: falliSubiti})
+
+}
+
+
+console.log(nomi_Squadre)
+
+
 
 
